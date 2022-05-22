b2a46f166b886d8b6fe639b5944e03ddb68860ee
Krutika-Muley <91215900+Krutika-Muley@users.noreply.github.com>
2022-05-22 18:55:43 +0530

    Update Statistics.cs

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -5,8 +5,31 @@ namespace Statistics
 {
     public class StatsComputer
     {
-        public Stats CalculateStatistics(List<float> numbers) {
-            //Implement statistics here
+         public void CalculateStatistics(List<float> numbers)
+        {
+            var numbersArray = numbers.ToArray();
+            int length = numbersArray.Length;
+           bool isEmpty = (length == 0);
+            if (isEmpty)
+            {
+                average = Double.NaN;
+                min = Double.NaN;
+                max = Double.NaN;
+            }
+            else
+            {
+                var sum = numbersArray[0];
+                 min = numbersArray[0];
+                 max = numbersArray[0];
+                for (int i = 1; i < length; i++)
+                {
+                    sum += numbersArray[i];
+                    min = Math.Min(min, numbersArray[i]);
+                    max = Math.Max(max, numbersArray[i]);
+                }
+                average = sum / numbersArray.Length;
+            }
         }
+        
     }
 }
