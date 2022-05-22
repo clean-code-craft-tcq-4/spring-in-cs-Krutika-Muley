09ac7e7846c3f135d04fffa361093ae110bccb4e
Krutika-Muley <91215900+Krutika-Muley@users.noreply.github.com>
2022-05-23 00:00:03 +0530

    Update StatsUnitTest.cs

--- Statistics.Test/StatsUnitTest.cs
+++ Statistics.Test/StatsUnitTest.cs
@@ -39,7 +39,7 @@ namespace Statistics.Test
 
             const float maxThreshold = 10.2;
             var statsAlerter = new StatsAlerter(maxThreshold, alerters);
-            statsAlerter.checkAndAlert(new List<float>{(float)0.2, (float)11.9,(float)4.3, (float)8.5});
+            statsAlerter.checkAndAlert(new List<float>{0.2f, 11.9f,4.3f,8.5f});
 
             Assert.True(emailAlert.emailSent);
             Assert.True(ledAlert.ledGlows);
