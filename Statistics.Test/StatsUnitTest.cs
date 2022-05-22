011687416071a9f483775cf1ce70fe33bfcaa0d4
Krutika-Muley <91215900+Krutika-Muley@users.noreply.github.com>
2022-05-22 19:35:41 +0530

    Update StatsUnitTest.cs

--- Statistics.Test/StatsUnitTest.cs
+++ Statistics.Test/StatsUnitTest.cs
@@ -10,8 +10,8 @@ namespace Statistics.Test
         public void ReportsAverageMinMax()
         {
             var statsComputer = new StatsComputer();
-            var computedStats = statsComputer.CalculateStatistics(
-                new List<___>{1.5, 8.9, 3.2, 4.5});
+             statsComputer.CalculateStatistics(
+                new List<float>{(float)1.5, (float)8.9, (float)3.2, (float)4.5});
             float epsilon = 0.001F;
             Assert.True(Math.Abs(statsComputer.average - 4.525) <= epsilon);
             Assert.True(Math.Abs(statsComputer.max - 8.9) <= epsilon);
@@ -21,11 +21,9 @@ namespace Statistics.Test
         public void ReportsNaNForEmptyInput()
         {
             var statsComputer = new StatsComputer();
-            var computedStats = statsComputer.CalculateStatistics(
-                new List<___>{});
-            //All fields of computedStats (average, max, min) must be
-            //Double.NaN (not-a-number), as described in
-            //https://docs.microsoft.com/en-us/dotnet/api/system.double.nan?view=netcore-3.1
+            statsComputer.CalculateStatistics(
+                new List<float>{});
+   
             Assert.IsNaN(statsComputer.average);
             Assert.IsNaN(statsComputer.max);
             Assert.IsNaN(statsComputer.min);
